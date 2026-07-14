b231744d190abca9d142757d918c16da786135a4
AlmikSS <ulmasoviskandar7@gmail.com>
2026-07-14 16:27:21 +0300

    Update GamePlayBootstrap.cs

--- Assets/_KONTUR - Simulation/_Scripts/Infrastructure/GamePlayBootstrap.cs
+++ Assets/_KONTUR - Simulation/_Scripts/Infrastructure/GamePlayBootstrap.cs
@@ -1,6 +1,4 @@
 ﻿using _KONTUR___Simulation._Scripts.DevConsole;
-using _KONTUR___Simulation._Scripts.GamePlay.NPC;
-using _KONTUR___Simulation._Scripts.GamePlay.NPC.Brain;
 using KofeyekToolkit.DevConsole;
 using KofeyekToolkit.LifeCycle;
 using UnityEngine;
@@ -10,7 +8,6 @@ namespace _KONTUR___Simulation._Scripts
     public sealed class GamePlayBootstrap : MonoBehaviour
     {
         [SerializeField] private DevConsoleAdapter _devConsoleAdapter;
-        [SerializeField] private NpcBrain _enemyPrefab;
         [SerializeField] private Transform[] _waypoints;
         
         private SpawnService _spawnService;
@@ -18,12 +15,6 @@ namespace _KONTUR___Simulation._Scripts
         public void Initialize()
         {
             _devConsoleAdapter.Initialize();
-            _spawnService = ServiceLocator.Get<SpawnService>();
-            
-            var playerTransform = GameObject.FindWithTag("Player").transform;
-            var enemyFactory = new EnemyFactory(_enemyPrefab, _waypoints, _spawnService, playerTransform);
-            enemyFactory.Create(new Vector3(0, 1, 30), Quaternion.identity);
-            
             CommandsRegistry.RegisterAllCommands();
         }
     }

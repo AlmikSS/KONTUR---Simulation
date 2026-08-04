aec10193e9403cbb2dd56580a8bfa8f21786deab
AlmikSS <ulmasoviskandar7@gmail.com>
2026-08-04 19:58:01 +0300

    SceneManagement

---
+++ Assets/_KONTUR - Simulation/_Scripts/Infrastructure/SceneManagement/SceneService.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace _KONTUR___Simulation._Scripts.SceneManagement
+{
+    public sealed class SceneService : MonoBehaviour
+    {
+        [SerializeField] private string _loadSceneName;
+        [SerializeField] private string _firstLevelSceneName;
+
+        private Coroutine _loadSceneRoutine;
+        
+        public static SceneService Instance { get; private set; }
+        
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+                LoadScene(_firstLevelSceneName);
+                return;
+            }
+            
+            Destroy(gameObject);
+        }
+
+        public void ReloadScene()
+        {
+            LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+        public void LoadScene(string sceneName)
+        {
+            if (_loadSceneRoutine != null)
+                StopCoroutine(_loadSceneRoutine);
+            
+            _loadSceneRoutine = StartCoroutine(LoadSceneRoutine(sceneName));
+        }
+
+        private IEnumerator LoadSceneRoutine(string sceneName)
+        {
+            var loadSceneAsyncOp = SceneManager.LoadSceneAsync(_loadSceneName);
+            while(!loadSceneAsyncOp.isDone)
+                yield return null;
+            
+            var sceneAsyncOp = SceneManager.LoadSceneAsync(sceneName);
+            sceneAsyncOp.allowSceneActivation = false;
+            
+            while(!sceneAsyncOp.isDone)
+                yield return null;
+            
+            sceneAsyncOp.allowSceneActivation = true;
+        }
+    }
+}

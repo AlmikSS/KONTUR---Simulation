78bdc8bafee0626bb0b413eb3f18b7f789d46b5e
AlmikSS <ulmasoviskandar7@gmail.com>
2026-08-06 23:52:29 +0300

    1

---
+++ Assets/_KONTUR - Simulation/_Scripts/GamePlay/UI/CreditsScroller.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class CreditsScroller : MonoBehaviour
+{
+    [Header("UI")]
+    [SerializeField] private TextMeshProUGUI creditsText;
+    [SerializeField] private float scrollSpeed = 50f;
+    [SerializeField] private float startDelay = 1f;
+
+    private RectTransform textRect;
+    private float textHeight;
+    private float startY;
+
+    private void Start()
+    {
+        textRect = creditsText.GetComponent<RectTransform>();
+        
+        creditsText.text = 
+            "<size=60><b>Mindcell</b></size>\n\n" +
+            "<size=36>Candy Candle Games</size>\n\n\n" +
+            "<size=32>Вова (Kannon)</size>\n" +
+            "UI/UX Artist, Frontend Programmer, Level Designer, Sound Designer\n\n\n" +
+            "<size=32>Ис (Kofeyek)</size>\n" +
+            "Lead, Programmer, Architecture Author\n\n\n" +
+            "<size=32>Женя (Eugene_Volsky)</size>\n" +
+            "3D Artist, Game Designer, Idea Author\n\n\n" +
+            "<size=32>CCGAI</size>\n" +
+            "Consultant";
+
+        creditsText.alignment = TextAlignmentOptions.Center;
+        
+        // Принудительно обновляем layout чтобы получить правильную высоту
+        Canvas.ForceUpdateCanvases();
+        textHeight = textRect.rect.height;
+        
+        // Стартовая позиция - текст внизу за экраном
+        startY = -Screen.height * 0.5f - textHeight * 0.5f;
+        textRect.anchoredPosition = new Vector2(0, startY);
+        
+        StartCoroutine(ScrollCredits());
+    }
+
+    private IEnumerator ScrollCredits()
+    {
+        yield return new WaitForSeconds(startDelay);
+        
+        // Конечная позиция - текст уходит вверх за экран
+        float endY = Screen.height * 0.5f + textHeight * 0.5f;
+        
+        while (textRect.anchoredPosition.y < endY)
+        {
+            textRect.anchoredPosition += Vector2.up * scrollSpeed * Time.deltaTime;
+            yield return null;
+        }
+        
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/_KONTUR - Simulation/_Scripts/GamePlay/UI/VHSNoise.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class VHSNoise : MonoBehaviour
+{
+    [SerializeField] private float noiseIntensity = 0.05f;
+    [SerializeField] private float scanlineSpeed = 2f;
+    
+    private Image bgImage;
+    private float scanlineOffset;
+    
+    void Start()
+    {
+        bgImage = GetComponent<Image>();
+    }
+    
+    void Update()
+    {
+        // Лёгкое мерцание фона
+        float noise = 1f - Random.Range(0f, noiseIntensity);
+        bgImage.color = new Color(noise, noise, noise);
+        
+        // Симуляция строк развёртки (scanlines)
+        scanlineOffset += Time.deltaTime * scanlineSpeed;
+        if (Mathf.Sin(scanlineOffset * 10f) > 0.95f)
+        {
+            bgImage.color = new Color(0.02f, 0.02f, 0.02f);
+        }
+    }
+}

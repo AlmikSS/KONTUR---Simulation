f8c9834fce10354a4bcf422a059b277529d5f185
AlmikSS <ulmasoviskandar7@gmail.com>
2026-08-06 20:52:05 +0300

    1

--- Assets/_KONTUR - Simulation/_Scripts/Infrastructure/SceneManagement/SceneState.cs
+++ Assets/_KONTUR - Simulation/_Scripts/Infrastructure/SceneManagement/SceneState.cs
@@ -3,13 +3,13 @@ using _KONTUR___Simulation._Scripts.GamePlay.UI.Laboratory;
 public sealed class SceneState
 {
     public DeathPayload Death { get; set; }
-    public int CurrentLevel { get; set; }
+    public int CurrentLevel { get; set; } = 1;
     public DayConfig DayConfig { get; set; }
 
     public void Clear()
     {
         Death = null;
-        CurrentLevel = 0;
+        CurrentLevel = 1;
     }
 }
 
